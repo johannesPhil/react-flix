2636efb471aa8d9cf92f3d1a400801d7b0153d30
John Peter <jay3pee@gmail.com>
2021-03-17 13:20:11 +0100

    added redux devtool extension

--- src/store.js
+++ src/store.js
@@ -1,13 +1,14 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const middleWare = [thunk];
 const movieStore = createStore(
 	rootReducer,
-	compose(
-		applyMiddleware(...middleWare),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+	composeEnhancers(
+		applyMiddleware(...middleWare)
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 );
 
