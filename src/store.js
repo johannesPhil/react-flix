a36f966a2d84192329cca3d8dcc5828387d9491d
John Peter <jay3pee@gmail.com>
2021-03-17 14:38:10 +0100

    production redux

--- src/store.js
+++ src/store.js
@@ -4,12 +4,18 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
 const middleWare = [thunk];
+
+const composeEnhancers =
+	typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+				// Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+		  })
+		: compose;
+
 const movieStore = createStore(
 	rootReducer,
-	composeEnhancers(
-		applyMiddleware(...middleWare)
-		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	)
+	composeEnhancers(applyMiddleware(...middleWare))
 );
 
 export default movieStore;
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
